3c9a7c9ba9b5d98a666920c88f26070512d129cd
Olivier Van Acker <cyberroadie@gmail.com>
2012-05-07 19:31:01 +0100

    add fractal go example

--- go-fractal/fern.go
+++ go-fractal/fern.go
@@ -13,9 +13,9 @@ import (
 
 func main() {
 	m := image.NewRGBA(image.Rect(0, 0, 400, 400))
-    blue := color.RGBA{0, 0, 0, 255}
+    blue := color.RGBA{0, 0, 0, 0}
     draw.Draw(m, m.Bounds(), &image.Uniform{blue}, image.ZP, draw.Src)
-	drawFern(m, 400, 400, 10000)
+	drawFern(m, 400, 400, 100000)
 	f, err := os.OpenFile("fern.png", os.O_CREATE | os.O_WRONLY, 0666)
 	if(err != nil) {
 		log.Fatal(err)
@@ -31,7 +31,7 @@ func drawFern(m *image.RGBA, x float32, y float32, steps int) {
 		x, y = transform(x, y)
 		drawPoint(m, x, y)
 		drawFern(m, x, y, steps - 1)
-	} 
+	}
 }
 
 func drawPoint(m *image.RGBA, x float32, y float32) {
@@ -41,7 +41,7 @@ func drawPoint(m *image.RGBA, x float32, y float32) {
 	scale := float32(height / 11)
     y = (height - 25) - (scale * y)
     x = (width / 2) + (scale * x)
-	m.Set(int(x), int(y), color.RGBA{0, 255, 0, 255})	
+	m.Set(int(x), int(y), color.RGBA{0, 255, 0, 255})
 }
 
 func transform(x float32, y float32) (float32, float32) {
@@ -55,6 +55,10 @@ func transform(x float32, y float32) (float32, float32) {
 	return x, y
 }
 
+type fern {
+
+}
+
 func transformPoint(x, y, a, b, c ,d , s float32) (float32, float32) {
 	return ((a * x) + (b * y)), ((c * x) + (d * y) + s)
-} 
+}
